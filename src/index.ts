89dcf51f59562ca70836755a1a2e73deec513316
WoonHaKim <work@woonakim.info>
2025-08-21 20:12:00 +0900

    fix: build deps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import IVSPlayer from './IVSPlayer';
 
 export default IVSPlayer;
-export { IVSPlayerProps, IVSPlayerRef } from './IVSPlayer';
+export type { IVSPlayerProps } from './IVSPlayer';
+export type { IVSPlayerRef } from './types';
 export * from './types';
 export * from './enums';
